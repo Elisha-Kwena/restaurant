26e853e2893532702b22bbc8331e30265acbd2af
Elisha Kwena <hatblack9874@gmail.com>
2025-10-27 12:13:18 +0300

    Improve profile sidebar and personal info UI
    Added responsive sidebar toggling for mobile view in the profile page, including a close button. Enhanced the personal info form with a profile picture upload UI, better layout, and added discard/save buttons for improved user experience.

--- foodie/src/app/(main)/profile/page.tsx
+++ foodie/src/app/(main)/profile/page.tsx
@@ -1,4 +1,6 @@
 "use client"
+
+import { useState } from "react"
 import Image from "next/image"
 import { useSidebar } from "@/contexts/SidebarContext"
 import Link from "next/link"
@@ -7,24 +9,35 @@ import LocalMallRoundedIcon from '@mui/icons-material/LocalMallRounded';
 import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
 import LockRoundedIcon from '@mui/icons-material/LockRounded';
 import WidgetsRoundedIcon from '@mui/icons-material/WidgetsRounded';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 
 import PersonalInfo from "@/components/pages/profile/Personal";
 export default function UserProfile(){
     const {isOpen} = useSidebar()
+    const [sidebarOpen, setSidebarOpen] = useState<boolean>(false)
+
+    const toggleSidebarOpen = () =>{
+        setSidebarOpen((prev) =>!prev)
+    }
     return(
         <>
             <div className={`md:min-h-screen bg-gray-200 md:pt-20 overflow-scroll transition-all duration-700 ease-in-out ${isOpen?"md:ml-84 ml-0ss":"ml-0 md:ml-18"}`}>
-                <div className={`h-screen bg-gray-200 flex flex-col gap-2 fixed right-1 overflow-scroll pb-22 transition-all duration-700 ease-in-out ${isOpen?"md:left-84 left-0":"md:left-18 left-0"}`}>
-                    <div className="mx-auto w-[80%] h-full flex items-start justify-between gap-4">
+                <div className={`h-screen bg-gray-200 flex flex-col gap-2 fixed right-1 overflow-scroll md:pb-22 pb-18 transition-all duration-700 ease-in-out ${isOpen?"md:left-84 left-0":"md:left-18 left-0"}`}>
+                    <div className="mx-auto w-full md:w-[80%] h-full flex items-start justify-between gap-4 p-1">
                         {/* sidebar */}
-                        <div className="w-[30%] flex flex-col items-start rounded dhadow bg-white h-full p-2">
-                            <div className="w-full h-46 relative rounded-full overflow-hidden">
+                        <div className={`w-3/4 md:w-[30%] fixed md:relative md:flex flex-col items-start rounded shadow bg-white h-full p-2 transition-all duration-700 ease-in-out ${sidebarOpen ? "left-0 md:clear-left":"-left-[500px] md:left-0"}`}>
+                            <div className="w-full h-46 relative rounded-full ">
                                 <Image
                                     src="/user.png"
                                     fill
                                     alt="user"
                                     className="object-center object-contain"
                                 />
+                                <button
+                                onClick={toggleSidebarOpen}
+                                className="absolute md:hidden w-10 h-10 rounded bg-gray-500 top-0 right-0 flex items-center justify-center">
+                                    <CloseRoundedIcon className="text-orange-600 text-[28px]!"/>
+                                </button>
                             </div>
                             <div className="w-full">
                                 <h1 className="text-black font-extrabold text-[20px] text-center">RedHat_109</h1>
@@ -54,7 +67,9 @@ export default function UserProfile(){
 
                         <div className="flex-1 flex flex-col items-start rounded shadow bg-white h-full p-2 px-4">
                             <div className="w-full flex items-center justify-start">
-                                <button className="cursor-pointer flex items-center justify-center hover:text-orange-600">
+                                <button
+                                onClick={toggleSidebarOpen}
+                                className="cursor-pointer flex md:hidden items-center justify-center hover:text-orange-600">
                                     <WidgetsRoundedIcon className="text-[30px]!"/>
                                 </button>
                             </div>
--- foodie/src/components/pages/profile/Personal.tsx
+++ foodie/src/components/pages/profile/Personal.tsx
@@ -1,9 +1,19 @@
+"use client"
+
+// interface ProfileProps{
+//     toggleSidebarOpen:()=> void;
+//     sidebarOpen:boolean
+// }
+import Image from "next/image"
+import CameraAltRoundedIcon from '@mui/icons-material/CameraAltRounded';
+
 export default function PersonalInfo(){
     return(
         <>
-            <div className="w-full">
-                <form className="w-full flex flex-col gap-6">
-                    <div className="w-full flex items-center justify-between">
+            <div className="w-full h-full">
+                <form className="w-full flex flex-col gap-6 h-full">
+                    <div className="w-full flex flex-col flex-1 gap-2 pl-1">
+                        <div className="w-full flex items-center justify-between">
                         <div className="flex items-center justify-start gap-6">
                             <div className="flex items-center justify-start gap-4">
                                 <input type="radio" id="sex" name="sex"/>
@@ -15,12 +25,23 @@ export default function PersonalInfo(){
                             </div>
                         </div>
                         <div className="flex items-center justify-start gap-4 bg-gray-500 rounded p-2">
-                            <label htmlFor="profile" className="text-white text-lg">Profile picture</label>
-                            <input type="file" id="profile" name="profile"/>  
+                            <label htmlFor="profile" className="text-white text-lg">
+                                <div className="w-32 h-32 rounded-full relative cursor-pointer">
+                                    <Image 
+                                        src="/user.png"
+                                        alt="user profile"
+                                        fill
+                                        className="object-center object-contain"
+                                    />
+                                    <div className="absolute w-14 h-14 z-100 flex items-center justify-center bottom-0 -right-4">
+                                        <CameraAltRoundedIcon className="text-white text-[28px]!"/>
+                                    </div>
+                                </div>
+                            </label>
+                            <input type="file" id="profile" name="profile" className="hidden"/>  
                         </div>
-                    </div>
-
-                    <div className="w-full grid grid-cols-2 gap-4">
+                        </div>
+                        <div className="w-full grid grid-cols-2 gap-4">
                         <div className="flex flex-col gap-1">
                             <label htmlFor="firstName">First Name :</label>
                             <input type="text" id="firstName" className="bg-gray-200 text-gray-600 p-3 px-4 font-bold rounded-full" placeholder="First Name :" />
@@ -33,18 +54,18 @@ export default function PersonalInfo(){
                             <label htmlFor="userName">UserName :</label>
                             <input type="text" id="userName" className="bg-gray-200 text-gray-600 p-3 px-4 font-bold rounded-full" placeholder="UserName :" />
                         </div>
-                    </div>
-                    <div className="w-full">
+                        </div>
+                        <div className="w-full">
                         <div className="flex flex-col gap-1">
                             <label htmlFor="email">Email :</label>
                             <input type="text" id="email" className="w-full bg-gray-200 text-gray-600 p-3 px-4 font-bold rounded-full" placeholder="Email :" />
                         </div>
-                        <div className="flex flex-col gap-1mt-4">
+                        <div className="flex flex-col gap-1 mt-4">
                             <label htmlFor="phone">Phone Number :</label>
                             <input type="tel" id="phone" className="w-full bg-gray-200 text-gray-600 p-3 px-4 font-bold rounded-full" placeholder="Phone Number :" />
                         </div>
-                    </div>
-                    <div className="w-full grid grid-cols-2 gap-4">
+                        </div>
+                        <div className="w-full grid grid-cols-2 gap-4">
                         <div className="flex flex-col gap-1">
                             <label htmlFor="address">Address :</label>
                             <input type="text" id="address" className="bg-gray-200 text-gray-600 p-3 px-4 font-bold rounded-full" placeholder="Address :" />
@@ -61,6 +82,11 @@ export default function PersonalInfo(){
                             <label htmlFor="house">House NO :</label>
                             <input type="text" id="house" className="bg-gray-200 text-gray-600 p-3 px-4 font-bold rounded-full" placeholder="(optional) :" />
                         </div>
+                        </div>
+                    </div>
+                    <div className="w-full flex items-center justify-between gap-4">
+                        <button className="w-1/2 p-3 rounded-md border-2 text-center font-bold border-orange-600 cursor-pointer text-orange-600 transition-all duration-300 ease-in-out hover:bg-orange-600 hover:text-white">Discard Changes</button>
+                        <button className="w-1/2 p-3 rounded-md border-2 text-center font-bold border-white bg-orange-600 text-white cursor-pointer transition-all duration-300 ease-in-out hover:bg-white hover:text-orange-600 hover:border-orange-600">Save Changes</button>
                     </div>
                 </form>
             </div>
